7e4c343f07b50dc0c56d0030594dd0e9b141bdcf
luffy sai ssll <36154333+sailao@users.noreply.github.com>
2020-02-16 16:57:31 +0630

    with mongodb

--- Controller/messageController.mjs
+++ Controller/messageController.mjs
@@ -1,21 +1,67 @@
+import mongodb from 'mongodb';
+
 const index = ({res}) => {
-    res.send('respond with a index');
+    mongodb.MongoClient.connect(process.env.DB_HOST, { useUnifiedTopology: true }, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("mydb");
+        dbo.collection("messages").find({text:{$exists: true}}, { projection: { _id: 1, text: 1 } }).toArray(function(err, result) {
+            if (err) throw err;
+            db.close();
+            res.json(result);
+        });
+    });
 }
 
 const show = (req, res) => {
-    res.send('respond with a show'+req.params.id);
+    mongodb.MongoClient.connect(process.env.DB_HOST, { useUnifiedTopology: true }, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("mydb");
+        dbo.collection("messages").findOne({"_id": mongodb.ObjectId(req.params.id)}, function(err, result) {
+            if (err) throw err;
+            res.json(result);
+            db.close();
+        });
+    });
 }
 
 const store = (req, res) => {
-    res.send('respond with a store'+req.body.title);
+    mongodb.MongoClient.connect(process.env.DB_HOST, { useUnifiedTopology: true }, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("mydb");
+        dbo.collection("messages").insertOne({"text": req.body.text}, function(err, result) {
+            if (err) throw err;
+            res.redirect(`/api/v1/messages`);
+            db.close();
+        });
+    });
 }
 
-const update = ({res}) => {
-    res.send('respond with a update');
+const update = (req, res) => {
+    Messages((message, db) =>{
+        message.deleteOne({"_id": mongodb.ObjectId(req.params.id)}, function(err, result) {
+            if (err) throw err;
+            db.close();
+        });
+    });
+    res.redirect(`/api/v1/messages/${req.params.id}`);
 }
 
-const destory = ({res}) => {
-    res.send('respond with a delete');
+const destory = (req, res) => {
+    Messages((message, db) =>{
+        message.deleteOne({"_id": mongodb.ObjectId(req.params.id)}, function(err, result) {
+            if (err) throw err;
+            db.close();
+        });
+    });
+    res.redirect(`/api/v1/messages`);
 }
 
+const Messages = init => {
+    mongodb.MongoClient.connect(process.env.DB_HOST, { useUnifiedTopology: true }, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("mydb");
+        var message = dbo.collection("messages");
+        init(message, db);
+    });
+}
 export default {index, show, store, update, destory};
